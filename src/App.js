e884d4052481831d93df1df7d7dbeb0a9eb71b72
Brandon-Torres <torres.brandon077@gmail.com>
2021-08-23 13:51:03 -0400

    mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar.js";
+import BodyPage from "./Components/BodyPage";
+
 //Hi my name is Alex
 function App() {
-  
+  const axios = require("axios");
+  const [books, setBooks] = useState([]);
+
+  async function getBooks() {
+    var bookRequest = {
+      method: "GET",
+      url: "https://www.googleapis.com/books/v1/volumes?",
+      params: { q: "anxiety" },
+    };
+    axios.request(bookRequest).then(function (response) {
+      setBooks(response.data.items);
+    });
+  }
+  useEffect(() => {
+    getBooks();
+  }, []);
+
+  console.log(books);
   return (
     <div>
-
+      <Navbar />
+      <BodyPage />
     </div>
   );
 }
---
+++ src/Components/BodyPage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import HomeBanner from "./Images/HomeBannerPic.jpg";
+import "./BodyPage.css";
+
+export default function ComplexGrid() {
+  return (
+    <div id="main-container">
+      <div id="Welcome-Message">
+        <h1>Welcome to Tranquility</h1>
+      </div>
+      <div id="home-banner"></div>
+      <div id="about-info">
+        aksdjgkajsda ga jaj'gakjg[akgjak aljgad;klgajdg]
+      </div>
+    </div>
+  );
+}
