e9fc8d4bc6f7bfc88293129160de7f3918c68cee
hector6921 <60336145+hector6921@users.noreply.github.com>
2021-08-23 16:32:46 -0400

    try

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 //Hi my name is Alex
 function App() {
   
+  const axios = require('axios')
+  const [books, setBooks] = useState([])
+
+  async function getBooks(){
+    var bookRequest = {
+      method: 'GET',
+      url: 'https://www.googleapis.com/books/v1/volumes?',
+      params: {q: 'anxiety'}
+    };
+    axios.request(bookRequest).then(function (response){
+      setBooks(response.data.items)
+    })
+  }
+  useEffect(()=>{getBooks()},[])
+
+console.log(books)
   return (
     <div>
 
