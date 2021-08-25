33ad96456b04e5648c8a840eaec46190f623e7a4
hector6921 <60336145+hector6921@users.noreply.github.com>
2021-08-25 18:14:02 -0400

    added api call for books

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import SendData from "./Components/Pages/supabase";
 
 
 function App() {
+
+  const axios = require("axios");
+  const [books, setBooks] = useState([]);
+
+  async function getBooks() {
+    var bookRequest = {
+      method: "GET",
+      url: "https://www.googleapis.com/books/v1/volumes?",
+      params: { q: "anxiety" },
+    };
+    axios.request(bookRequest).then(function (response) {
+      setBooks(response.data.items);
+    });
+  }
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <div>
       {/* <SignInSide /> */}
       <Content/>
-     
+     <img src={books[0].volumeInfo.imageLinks.smallThumbnail} />
     </div>
   );
 }
